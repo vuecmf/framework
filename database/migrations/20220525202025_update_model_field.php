b257932f19ae330bd6ca1ad6908fdb2e4cee0b51
vuecmf <tulihua2004@126.com>
2022-05-25 20:46:53 +0800

    admin表的IP类字段信息变更

---
+++ database/migrations/20220525202025_update_model_field.php
@@ -0,0 +1,23 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class UpdateModelField extends Migrator
+{
+
+    public function up()
+    {
+
+        //更新表数据
+        $this->execute('update '.config('database.connections.mysql.prefix').'model_field set type = "varchar",length = 24 where id in(74,76);');
+
+    }
+
+    public function down()
+    {
+        //还原表数据
+        $this->execute('update '.config('database.connections.mysql.prefix').'model_field set type = "bigint",length = 20 where id in(74,76);');
+
+    }
+}
